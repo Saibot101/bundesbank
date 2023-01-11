a01f71009200307e5dc9f906ce30ccecde83bfed
Tobias Maas <tobiasmaas98@web.de>
2023-01-11 23:21:19 +0100

    correct main

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import { router } from './router'
-import VNetworkGraph from "v-network-graph"
-import "v-network-graph/lib/style.css"
 //import VueApexCharts from "vue3-apexcharts"
 
 
@@ -13,5 +11,4 @@ loadFonts()
 createApp(App)
   .use(vuetify)
   .use(router)
-  .use(VNetworkGraph)
   .mount('#app')
